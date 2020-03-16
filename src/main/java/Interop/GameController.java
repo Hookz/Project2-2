57e45097a3621f6e7761aee0e32fef45cd3ed79d
Elliot <elliotdoe1999@gmail.com>
2020-03-16 12:37:46 +0100

    Constructor with Agent Initialisation

--- src/main/java/Interop/GameController.java
+++ src/main/java/Interop/GameController.java
@@ -1,12 +1,18 @@
 package Interop;
 
+import Group2.AgentsFactory;
 import Group2.Teleport;
+import Interop.Agent.Guard;
+import Interop.Agent.Intruder;
 import Interop.Percept.Scenario.*;
 import Interop.Geometry.*;
 import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
-class GameController{
+public class GameController{
 
     private ScenarioPercepts scenarioPercept;
     private ScenarioIntruderPercepts scenarioIntruderPercepts;
@@ -39,6 +45,13 @@ class GameController{
     private List<Rectangle> doors;
     private List<Rectangle> windows;
     private List<Rectangle> sentries;
+    private HashMap<Intruder,Ellipse2D> intruderLocations;
+    private HashMap<Guard,Ellipse2D> guardLocations;
+    private HashMap<Intruder,Direction> intruderDirections;
+    private HashMap<Guard,Direction> guardDirections;
+
+    List<Intruder> intruders;
+    List<Guard> guards;
 
     public GameController(int gameMode, int height, int width, int numGuards, int numIntruders, double captureDistance, int winConditionIntruderRounds, double maxRotationAngle, double maxMoveDistanceIntruder,
                               double maxSprintDistanceIntruder, double maxMoveDistanceGuard, int sprintCooldown, int pheromoneCooldown, double radiusPheromone, double slowDownModifierWindow,
@@ -80,6 +93,50 @@ class GameController{
         this.doors = doors;
         this.windows = windows;
         this.sentries = sentries;
+
+        /* THIS IS FOR PERIOD 1 AS WE ONLY NEED AN EXPLORATION AGENT
+        Later there needs to be a functionality, such that we can choose which groups intruder, and which groups guard
+        to use in the game.
+
+        Also based on assumption that there will be 1 spawnArea per group
+         */
+
+        //Intruder Agent Constuctor
+        Random random = new Random();
+        if(numIntruders>0) {
+            intruders = AgentsFactory.createIntruders(numIntruders);
+            for (Intruder intruder : intruders) {
+                double x = spawnAreaIntruders.get(0).getX() + spawnAreaIntruders.get(0).getWidth() * random.nextDouble();
+                double y = spawnAreaIntruders.get(0).getY() + spawnAreaIntruders.get(0).getHeight() * random.nextDouble();
+                Ellipse2D intruderLocation = new Ellipse2D.Double(x, y, 0.5, 0.5);
+                intruderLocations.put(intruder, intruderLocation);
+
+                double directionAngle = (2*Math.PI) * random.nextDouble();
+                Direction direction = Direction.fromRadians(directionAngle);
+                intruderDirections.put(intruder,direction);
+            }
+        }
+
+        //Guard Agent Contructor
+        if(numGuards>0) {
+            guards = AgentsFactory.createGuards(numGuards);
+            for (Guard guard : guards) {
+                double x = spawnAreaGuards.get(0).getX() + spawnAreaGuards.get(0).getWidth() * random.nextDouble();
+                double y = spawnAreaGuards.get(0).getY() + spawnAreaGuards.get(0).getHeight() * random.nextDouble();
+                Ellipse2D guardLocation = new Ellipse2D.Double(x, y, 0.5, 0.5);
+                guardLocations.put(guard, guardLocation);
+
+                double directionAngle = (2*Math.PI) * random.nextDouble();
+                Direction direction = Direction.fromRadians(directionAngle);
+                guardDirections.put(guard,direction);
+            }
+        }
+
+        //Run the UI construction
+        //UI.createUI(<all the walls etc above here>)
+
+
+
     }
 
 
