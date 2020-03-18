8e3d750b0dfbd8278329b8830be3ee73ba4ba1c9
Michal Jarski <michal@jarski.eu>
2020-03-18 09:47:11 +0100

    Parameterise the stepSize in GameController.checkIfLegalMove
    Signed-off-by: Michal Jarski <michal@jarski.eu>

--- src/main/java/Interop/GameController.java
+++ src/main/java/Interop/GameController.java
@@ -93,6 +93,8 @@ public class GameController{
     private HashMap<Guard, AreaPercepts> guardsAreaPercepts;
     private HashMap<Intruder, AreaPercepts> intruderAreaPercepts;
 
+    private final double COLLISION_CHECK_STEP_SIZE = 0.05;
+
     public GameController(int gameMode, int height, int width, int numGuards, int numIntruders, double captureDistance, int winConditionIntruderRounds, double maxRotationAngle, double maxMoveDistanceIntruder,
                               double maxSprintDistanceIntruder, double maxMoveDistanceGuard, int sprintCooldown, int pheromoneCooldown, double radiusPheromone, double slowDownModifierWindow,
                               double slowDownModifierDoor, double slowDownModifierSentryTower, double viewAngle, int viewRays, double viewRangeIntruderNormal, double viewRangeIntruderShaded,
@@ -282,7 +284,7 @@ public class GameController{
             double newX = guardLocation.getX() + Math.cos(guardDirection.getDegrees()) * ((Move) action).getDistance().getValue();
             double newY = guardLocation.getY() + Math.sin(guardDirection.getDegrees()) * ((Move) action).getDistance().getValue();
             Ellipse2D newGuardLocation = new Ellipse2D.Double(newX,newY,guardLocation.getWidth(),guardLocation.getHeight());
-            if(checkIfLegalMove(guardLocation,newGuardLocation)) {
+            if(checkIfLegalMove(guardLocation,newGuardLocation,COLLISION_CHECK_STEP_SIZE)) {
                 //Teleporting functionality and limitation
                 for(Teleport teleport:teleports) {
                     //Leaving a teleporter area, remove flag
@@ -338,7 +340,7 @@ public class GameController{
             double newX = intruderLocation.getX() + Math.cos(intruderDirection.getDegrees()) * ((Move) action).getDistance().getValue();
             double newY = intruderLocation.getY() + Math.sin(intruderDirection.getDegrees()) * ((Move) action).getDistance().getValue();
             Ellipse2D newintruderLocation = new Ellipse2D.Double(newX,newY,intruderLocation.getWidth(),intruderLocation.getHeight());
-            if(checkIfLegalMove(intruderLocation,newintruderLocation)) {
+            if(checkIfLegalMove(intruderLocation,newintruderLocation, COLLISION_CHECK_STEP_SIZE)) {
                 //Teleporting functionality and limitation
                 for(Teleport teleport:teleports) {
                     //Leaving a teleporter area, remove flag
@@ -371,7 +373,7 @@ public class GameController{
                 double newY = intruderLocation.getY() + Math.sin(intruderDirection.getDegrees()) * ((Move) action).getDistance().getValue();
                 Ellipse2D newintruderLocation = new Ellipse2D.Double(newX, newY, intruderLocation.getWidth(), intruderLocation.getHeight());
 
-                if (checkIfLegalMove(intruderLocation, newintruderLocation)) {
+                if (checkIfLegalMove(intruderLocation, newintruderLocation, COLLISION_CHECK_STEP_SIZE)) {
                     //Teleporting functionality and limitation
                     for(Teleport teleport:teleports) {
                         //Leaving a teleporter area, remove flag
@@ -415,7 +417,7 @@ public class GameController{
     }
 
 	private boolean checkIfLegalMove(Ellipse2D initialLocation,
-			Ellipse2D newLocation) {
+			Ellipse2D newLocation, double stepSize) {
 		// Check for collisions. If collision occurs, return false and skip turn
 
         if((newLocation.getX()-newLocation.getWidth()/2)<0||(newLocation.getX()+newLocation.getWidth()/2)>width||(newLocation.getY()-newLocation.getHeight()/2)<0||(newLocation.getY()+newLocation.getHeight()/2)>height)
@@ -432,7 +434,6 @@ public class GameController{
 		Point2D current;
 		Ellipse2D temp_agent = initialLocation;
 		int counter = 0;
-		double stepSize = 0.05;
 		while (temp_agent.getX()<newLocation.getX()&&temp_agent.getY()<newLocation.getY()) {
 		    ++counter;
 			current = line.evalAtX(initialLocation.getX()+stepSize*counter);
