518c613323b59772418fd5d8b4e6963601842c84
Arda Ntourali <a.ntourali@student.maastrichtuniversity.nl>
2020-03-22 23:23:09 +0300

    First agent.

---
+++ src/main/java/Group2/Agents/Morontruder.java
@@ -0,0 +1,12 @@
+package Group2.Agents;
+
+import Interop.Action.GuardAction;
+import Interop.Agent.Guard;
+import Interop.Percept.GuardPercepts;
+
+public class Morontruder implements Guard {
+    public GuardAction getAction(GuardPercepts guardPercepts) {
+        //Return whatever action to take, depending on the perception we have under here.
+        return null;
+    }
+}
