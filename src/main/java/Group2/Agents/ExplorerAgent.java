829ad6f2226b545389ba9f8d3176ff39982b9c69
Elliot <elliotdoe1999@gmail.com>
2020-03-25 18:46:25 +0100

    Morontruder agent

--- src/main/java/Group2/Agents/ExplorerAgent.java
+++ src/main/java/Group2/Agents/ExplorerAgent.java
@@ -8,40 +8,36 @@ import Interop.Agent.Intruder;
 import Interop.Geometry.Angle;
 import Interop.Geometry.Direction;
 import Interop.Percept.IntruderPercepts;
+import Interop.Percept.Vision.ObjectPercept;
+import Interop.Percept.Vision.ObjectPerceptType;
 import Interop.Utils.Utils;
+
+import java.util.Random;
+import java.util.Set;
 
 public class ExplorerAgent implements Intruder {
 
-    int sprintCooldown = 0;
+    int counter = 0;
     @Override
     public IntruderAction getAction(IntruderPercepts percepts) {
 
-        Direction target = percepts.getTargetDirection();
-        System.out.println("Target Angle: "+target.getDegrees());
-        if(target.getDegrees()>0.001||target.getDegrees()<359.999){
-            if((Math.abs(target.getDegrees()-90))<=percepts.getScenarioIntruderPercepts().getScenarioPercepts().getMaxRotationAngle().getDegrees()){
-                double turn = -1*(90-target.getDegrees());
-                System.out.println("Alligning with Target");
-                System.out.println("Turning "+turn+" degrees to allign");
-                return new Rotate(Angle.fromDegrees(turn));
-            }else{
-                if (target.getDegrees()<90) {
-                    System.out.println("Max Rotation");
-                    return new Rotate(Angle.fromDegrees(-1 * percepts.getScenarioIntruderPercepts().getScenarioPercepts().getMaxRotationAngle().getDegrees()));
-                }else{
-                    System.out.println("Max Rotation");
-                    return new Rotate(Angle.fromDegrees(percepts.getScenarioIntruderPercepts().getScenarioPercepts().getMaxRotationAngle().getDegrees()));
-                }
-            }
-        }else{
-            if(sprintCooldown==0){
-                sprintCooldown = percepts.getScenarioIntruderPercepts().getSprintCooldown();
-                return new Sprint(percepts.getScenarioIntruderPercepts().getMaxSprintDistanceIntruder());
-            }else{
-                sprintCooldown--;
-                return new Move(percepts.getScenarioIntruderPercepts().getMaxMoveDistanceIntruder());
-            }
-        }
+        Direction targetDirection = Direction.fromDegrees(percepts.getTargetDirection().getDegrees()%360);
+        Angle rotationAngle = targetDirection.getDistance(Angle.fromDegrees(90));
+        Angle maxRotationAngle = percepts.getScenarioIntruderPercepts().getScenarioPercepts().getMaxRotationAngle();
 
+        if(rotationAngle.getDegrees() > maxRotationAngle.getDegrees())
+            rotationAngle = maxRotationAngle;
+
+
+        if(targetDirection.getDegrees() < 90 && targetDirection.getDegrees() > 270)
+            rotationAngle = Angle.fromDegrees(-rotationAngle.getDegrees());
+
+        if(counter == 1){
+            counter = 0;
+            return new Rotate(Angle.fromDegrees(rotationAngle.getDegrees()));
+        }else{
+            counter++;
+            return new Move(percepts.getScenarioIntruderPercepts().getMaxMoveDistanceIntruder());
+        }
     }
 }
